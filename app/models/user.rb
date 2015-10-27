0854f41700d3ac6cc5e4df48c4f728b3c669365e
Danila Ermakov <dnlrmkv@gmail.com>
2015-10-27 19:33:53 +0300

    Add user to project. Create user model, controller & sign up page

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+
+	validates :password, confirmation: true
+	validates :username, :email, :password, presence: true
+	validates :username, :email, uniqueness: true
+
+end
